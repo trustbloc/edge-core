df9f7bff663605af620b503a345dc96a8173676c
George Aristy <george.aristy@securekey.com>
2020-11-02 10:36:42 -0500

    feat: #97 zcapld: create API
    Signed-off-by: George Aristy <george.aristy@securekey.com>

---
+++ pkg/zcapld/create.go
@@ -0,0 +1,233 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package zcapld
+
+import (
+	"crypto/rand"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/proof"
+	ariessigner "github.com/hyperledger/aries-framework-go/pkg/doc/signature/signer"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/verifiable"
+)
+
+// ParseCapability parses a Capability.
+func ParseCapability(raw []byte) (*Capability, error) {
+	zcap := &Capability{}
+
+	err := json.Unmarshal(raw, zcap)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal zcap: %w", err)
+	}
+
+	return zcap, nil
+}
+
+// CapabilityOptions configures capabilities.
+type CapabilityOptions struct {
+	ID               string
+	Parent           string
+	Invoker          string
+	Controller       string
+	Delegator        string
+	AllowedAction    []string
+	InvocationTarget InvocationTarget
+	Challenge        string
+	Domain           string
+	CapabilityChain  []interface{}
+}
+
+// CapabilityOption configures CapabilityOptions.
+type CapabilityOption func(options *CapabilityOptions)
+
+// WithID overrides the default ID (urn:uuid:<uuid value>) used.
+func WithID(id string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.ID = id
+	}
+}
+
+// WithParent identifies the parent Capability.
+func WithParent(p string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.Parent = p
+	}
+}
+
+// WithInvoker identifies the invoker of the Capability.
+func WithInvoker(i string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.Invoker = i
+	}
+}
+
+// WithController identifies the controller of the Capability.
+func WithController(c string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.Controller = c
+	}
+}
+
+// WithDelegator identifies the delegator of the Capability.
+func WithDelegator(d string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.Delegator = d
+	}
+}
+
+// WithAllowedActions sets the actions allowed by the Capability.
+func WithAllowedActions(actions ...string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.AllowedAction = actions
+	}
+}
+
+// WithInvocationTarget sets the invocation target on the Capability.
+func WithInvocationTarget(targetID, targetType string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.InvocationTarget = InvocationTarget{
+			ID:   targetID,
+			Type: targetType,
+		}
+	}
+}
+
+// WithChallenge sets the challenge to include in the proof.
+func WithChallenge(c string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.Challenge = c
+	}
+}
+
+// WithDomain sets the domain to include in the proof.
+func WithDomain(d string) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.Domain = d
+	}
+}
+
+// WithCapabilityChain specifies the capabilityChain on the proof for the Capability.
+func WithCapabilityChain(chain ...interface{}) CapabilityOption {
+	return func(o *CapabilityOptions) {
+		o.CapabilityChain = chain
+	}
+}
+
+// Signer signs the Capability.
+type Signer struct {
+	ariessigner.SignatureSuite
+	SuiteType          string
+	VerificationMethod string
+}
+
+// NewCapability constructs a new, signed Capability with the options provided.
+func NewCapability(signer *Signer, options ...CapabilityOption) (*Capability, error) {
+	if signer == nil {
+		return nil, errors.New("must provide a signer")
+	}
+
+	opts := &CapabilityOptions{
+		ID: fmt.Sprintf("urn:uuid:%s", uuid.New().String()),
+	}
+
+	for i := range options {
+		options[i](opts)
+	}
+
+	zcap := &Capability{
+		Context:          SecurityContextV2,
+		ID:               opts.ID,
+		Invoker:          opts.Invoker,
+		Controller:       opts.Controller,
+		Delegator:        opts.Delegator,
+		Parent:           opts.Parent,
+		AllowedAction:    opts.AllowedAction,
+		InvocationTarget: opts.InvocationTarget,
+	}
+
+	err := signZCAP(zcap, signer, opts)
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign zcap: %w", err)
+	}
+
+	return zcap, nil
+}
+
+func signZCAP(zcap *Capability, signer *Signer, options *CapabilityOptions) error {
+	raw, err := json.Marshal(zcap)
+	if err != nil {
+		return fmt.Errorf("failed to marshal zcap: %w", err)
+	}
+
+	nonce, err := nonce()
+	if err != nil {
+		return fmt.Errorf("failed to generate a nonce: %w", err)
+	}
+
+	now := time.Now()
+	ldSigner := ariessigner.New(signer)
+
+	signedDoc, err := ldSigner.Sign(
+		&ariessigner.Context{
+			SignatureType:           signer.SuiteType,
+			SignatureRepresentation: proof.SignatureJWS,
+			Created:                 &now,
+			Domain:                  options.Domain,
+			Nonce:                   nonce,
+			VerificationMethod:      signer.VerificationMethod,
+			Challenge:               options.Challenge,
+			Purpose:                 ProofPurpose,
+			CapabilityChain:         options.CapabilityChain,
+		},
+		raw,
+	)
+	if err != nil {
+		return fmt.Errorf("document signer failed to sign zcap: %w", err)
+	}
+
+	zcap.Proof, err = parseProofs(signedDoc)
+	if err != nil {
+		return fmt.Errorf("failed to parse proof for zcap: %w", err)
+	}
+
+	return nil
+}
+
+func nonce() ([]byte, error) {
+	n := make([]byte, 64)
+
+	_, err := rand.Reader.Read(n)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from crypto/rand: %w", err)
+	}
+
+	return n, nil
+}
+
+func parseProofs(signedZcap []byte) ([]verifiable.Proof, error) {
+	rawProof := &struct {
+		Proof json.RawMessage `json:"proof,omitempty"`
+	}{}
+
+	err := json.Unmarshal(signedZcap, rawProof)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal zcap to extract proof: %w", err)
+	}
+
+	var proofs []verifiable.Proof
+
+	err = json.Unmarshal(rawProof.Proof, &proofs)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse proof: %w", err)
+	}
+
+	return proofs, nil
+}
---
+++ pkg/zcapld/create_test.go
@@ -0,0 +1,159 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package zcapld_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/jsonld"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/suite"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/signature/suite/ed25519signature2018"
+	ariesver "github.com/hyperledger/aries-framework-go/pkg/doc/signature/verifier"
+	"github.com/hyperledger/aries-framework-go/pkg/doc/verifiable"
+	"github.com/hyperledger/aries-framework-go/pkg/kms"
+	"github.com/stretchr/testify/require"
+
+	"github.com/trustbloc/edge-core/pkg/zcapld"
+)
+
+func TestParseCapability(t *testing.T) {
+	t.Run("parses a capability", func(t *testing.T) {
+		expected := &zcapld.Capability{
+			ID:            uuid.New().String(),
+			Context:       zcapld.SecurityContextV2,
+			Invoker:       uuid.New().String(),
+			Controller:    uuid.New().String(),
+			Delegator:     uuid.New().String(),
+			Parent:        uuid.New().String(),
+			AllowedAction: []string{uuid.New().String()},
+			InvocationTarget: zcapld.InvocationTarget{
+				ID:   uuid.New().String(),
+				Type: "urn:edv:document",
+			},
+			Proof: []verifiable.Proof{{
+				"type":               "Ed25519Signature2018",
+				"created":            "2020-10-07T21:59:06Z",
+				"verificationMethod": uuid.New().String(),
+				"proofPurpose":       "capabilityDelegation",
+				"capabilityChain":    []interface{}{uuid.New().String()},
+				"jws":                uuid.New().String(),
+			}},
+		}
+		result, err := zcapld.ParseCapability(marshal(t, expected))
+		require.NoError(t, err)
+		require.Equal(t, expected, result)
+	})
+
+	t.Run("fails if document is malformed", func(t *testing.T) {
+		_, err := zcapld.ParseCapability([]byte("{"))
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "failed to unmarshal zcap")
+	})
+}
+
+func TestNewCapability(t *testing.T) {
+	t.Run("creates new capability with proof", func(t *testing.T) {
+		expected := &zcapld.Capability{
+			ID:            uuid.New().String(),
+			Invoker:       uuid.New().String(),
+			Controller:    uuid.New().String(),
+			Delegator:     uuid.New().String(),
+			Parent:        uuid.New().String(),
+			AllowedAction: []string{uuid.New().String()},
+			InvocationTarget: zcapld.InvocationTarget{
+				ID:   uuid.New().String(),
+				Type: uuid.New().String(),
+			},
+		}
+		capabilityChain := []interface{}{fmt.Sprintf("urn:zcap:%s", uuid.New().String())}
+		signer := testSigner(t, kms.ED25519)
+		challenge := uuid.New().String()
+		domain := uuid.New().String()
+		verificationMethod := keyID(signer)
+		result, err := zcapld.NewCapability(
+			&zcapld.Signer{
+				SignatureSuite:     ed25519signature2018.New(suite.WithSigner(signer)),
+				SuiteType:          ed25519signature2018.SignatureType,
+				VerificationMethod: verificationMethod,
+			},
+			zcapld.WithID(expected.ID),
+			zcapld.WithParent(expected.Parent),
+			zcapld.WithInvoker(expected.Invoker),
+			zcapld.WithController(expected.Controller),
+			zcapld.WithDelegator(expected.Delegator),
+			zcapld.WithAllowedActions(expected.AllowedAction...),
+			zcapld.WithInvocationTarget(expected.InvocationTarget.ID, expected.InvocationTarget.Type),
+			zcapld.WithChallenge(challenge),
+			zcapld.WithDomain(domain),
+			zcapld.WithCapabilityChain(capabilityChain...),
+		)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.Equal(t, zcapld.SecurityContextV2, result.Context)
+		require.Equal(t, expected.ID, result.ID)
+		require.Equal(t, expected.Invoker, result.Invoker)
+		require.Equal(t, expected.Controller, result.Controller)
+		require.Equal(t, expected.Delegator, result.Delegator)
+		require.Equal(t, expected.Parent, result.Parent)
+		require.Equal(t, expected.AllowedAction, result.AllowedAction)
+		require.Equal(t, expected.InvocationTarget.ID, result.InvocationTarget.ID)
+		require.Len(t, result.Proof, 1)
+		proof := result.Proof[0]
+		require.Equal(t, ed25519signature2018.SignatureType, proof["type"])
+		require.Contains(t, proof, "created")
+		require.Equal(t, verificationMethod, proof["verificationMethod"])
+		require.Equal(t, zcapld.ProofPurpose, proof["proofPurpose"])
+		require.Equal(t, capabilityChain, proof["capabilityChain"])
+	})
+
+	t.Run("sets default ID", func(t *testing.T) {
+		signer := testSigner(t, kms.ED25519)
+		result, err := zcapld.NewCapability(&zcapld.Signer{
+			SignatureSuite:     ed25519signature2018.New(suite.WithSigner(signer)),
+			SuiteType:          ed25519signature2018.SignatureType,
+			VerificationMethod: keyID(signer),
+		})
+		require.NoError(t, err)
+		require.NotEmpty(t, result.ID)
+	})
+
+	t.Run("proof is verifiable", func(t *testing.T) {
+		signer := testSigner(t, kms.ED25519)
+		zcap, err := zcapld.NewCapability(&zcapld.Signer{
+			SignatureSuite:     ed25519signature2018.New(suite.WithSigner(signer)),
+			SuiteType:          ed25519signature2018.SignatureType,
+			VerificationMethod: keyID(signer),
+		})
+		require.NoError(t, err)
+		ver, err := ariesver.New(
+			zcapld.SimpleKeyResolver{keyID(signer): keyValue(t, signer)},
+			ed25519signature2018.New(suite.WithVerifier(ed25519signature2018.NewPublicKeyVerifier())),
+		)
+		require.NoError(t, err)
+		err = ver.Verify(marshal(t, zcap), jsonld.WithDocumentLoader(testLDDocumentLoader))
+		require.NoError(t, err)
+	})
+
+	t.Run("error: signer not provided", func(t *testing.T) {
+		_, err := zcapld.NewCapability(nil)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "must provide a signer")
+	})
+
+	t.Run("error: fails if signature suites are not provided", func(t *testing.T) {
+		signer := testSigner(t, kms.ED25519)
+		_, err := zcapld.NewCapability(&zcapld.Signer{
+			SignatureSuite:     ed25519signature2018.New(suite.WithSigner(signer)),
+			SuiteType:          "",
+			VerificationMethod: keyID(signer),
+		})
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "signature type is missing")
+	})
+}
